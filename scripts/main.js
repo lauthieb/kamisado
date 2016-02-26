71e94e7a05eb6c53f8c54667b745d4c4c72c4c92
Laurent THIEBAULT <thiebault.laurent@gmail.com>
2016-02-26 20:25:13 +0100

    Mise en place recherche cases dispos

--- scripts/main.js
+++ scripts/main.js
@@ -22,13 +22,13 @@ function placePawns() {
         whitePawn.addClass(colors[0][i]);
         whitePawn.addClass('white-pawn');
         whitePawn.click(function() {
-            showAllowedPlaces(getPosition($(this)));
+            showAllowedPlacesWhite(getPosition($(this)));
         });
         td.append(whitePawn);
     }
 
     for (var i=0; i<8; i++) {
-        var td = $('#h td:nth-child(' + (i+1) + ')');
+        var td = $('#g td:nth-child(' + (i+1) + ')');
         var blackPawn = $('<div></div>');
         blackPawn.attr('id', "b-" + colors[7][i]);
         blackPawn.addClass(colors[7][i]);
@@ -41,19 +41,31 @@ function getPosition(pawn) {
     return pawn.parent().attr('id');
 }
 
-function showAllowedPlaces(id) {
+function showAllowedPlacesWhite(id) {
     var ligne = id.substring(0,1).charCodeAt(0)-97;
     var col = id.substring(1,2);
+    var blackFound = false;
 
     for (var i=0; i<8; i++) {
         for (var j=0; j<8; j++) {
-            if (j == col) {
-                var td = $('#board tr:nth-child(' + (i+1) + ') ' + 'td:nth-child(' + (j+1) + ')');
-                if (i!= ligne && !td.children().first().hasClass('black-pawn')) {
+            var td = $('#board tr:nth-child(' + (i+1) + ') ' + 'td:nth-child(' + (j+1) + ')');
+            if (td.children().first().hasClass('black-pawn')) {
+                blackFound = true;
+            }
+            if (i!= ligne && !blackFound) {
+                /* Column */
+                if (j == col) {
+                    td.append('<p>OK</p>');
+                }
+                /* Left diagonal */
+                if(j-col == i) {
+                    td.append('<p>OK</p>');
+                }
+                /* Right diagonal */
+                if(i+j == col) {
                     td.append('<p>OK</p>');
                 }
             }
-
         }
     }
 }
